23d6a7e891342370bde50e424298b37fad9841eb
cyberDraziel <abhijith@cyberdude.com>
2018-12-19 13:22:41 +0530

    initial-commit

---
+++ music/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Album(models.Model):
+    artist = models.CharField(max_length=20)
+    album_title = models.CharField(max_length=20)
+    genre = models.CharField(max_length=20)
+    album_logo = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('music:details', kwargs={'pk': self.pk, })
+
+    def __str__(self):
+        return self.album_title+' '+self.artist
+
+    class Meta:
+        db_table = 'Album'
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=50)
+    file_type = models.CharField(max_length=10)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title+'.'+self.file_type
+
+    class Meta:
+        db_table = 'Song'
